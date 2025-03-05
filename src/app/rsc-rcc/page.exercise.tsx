5ca52d5b7d32df68d59039f92f416f560aa68e54
Mike Codeur <contact@mikecodeur.com>
2025-03-05 10:54:58 +0800

    exo

---
+++ src/app/rsc-rcc/page.exercise.tsx
@@ -0,0 +1,15 @@
+const Page = () => {
+  // 🐶 Détermine dans quel environnement s'exécute ce composant grâce à un log
+  // 🤖 `console.log('Je suis un développeur FullStack Next')`
+  //
+  // 🐶 Vérifie dans la console du navigateur et dans la console du serveur (terminal)
+
+  // 🐶 N'oublie pas de faire les exercices bonus
+  return (
+    <div>
+      <p>Je suis un développeur FullStack Next</p>
+    </div>
+  )
+}
+
+export default Page
---
+++ src/app/rsc-rcc/page.tsx
@@ -0,0 +1,6 @@
+//export {default} from "./page.final"
+
+export {default} from './page.exercise'
+
+// 1. 🚀 Convertir le composant en RCC (React Client Component)
+//export {default} from './page.bonus-1'
