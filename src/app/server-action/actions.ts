4c49ba0a020b5239217c4450acc34ee1b42bd00a
Mike Codeur <contact@mikecodeur.com>
2025-03-05 14:07:06 +0800

    init exo

--- src/app/server-action/actions.ts
+++ src/app/server-action/actions.ts
@@ -1,2 +1,19 @@
-export * from './actions.exercise'
-//export * from './actions.final'
+'use server'
+
+import {createUser, getUserByEmail} from '@/db'
+
+export async function addUser(name: string, email: string) {
+  if (!email || !name) {
+    return {error: 'Données invalides'}
+  }
+  const user = await getUserByEmail(email)
+  if (user) {
+    return {error: 'Utilisateur existe déjà'}
+  }
+  const userCreated = await createUser({
+    name,
+    email,
+    role: 'user',
+  })
+  return {success: true, user: userCreated}
+}
--- src/app/server-action/page.tsx
+++ src/app/server-action/page.tsx
@@ -1,2 +1,66 @@
-export {default} from './page.exercise'
-//export {default} from './page.final'
+'use client'
+import React, {useState} from 'react'
+
+import {Button} from '@/components/ui/button'
+import {Input} from '@/components/ui/input'
+import {Label} from '@/components/ui/label'
+
+import {addUser} from './actions'
+
+export default function Page() {
+  const [message, setMessage] = useState<string>('')
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('user@gmail.com')
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+    const result = await addUser(name, email)
+    if (result.error) {
+      setMessage(result.error)
+    } else {
+      setMessage(`utilisateur créé avec succès`)
+    }
+  }
+
+  return (
+    <div className="p-8">
+      <h1 className="mb-4 text-2xl font-bold">Crééer un utilisateur</h1>
+
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <div>
+          <Label htmlFor="email" className="mb-1 block text-sm font-medium">
+            Email
+          </Label>
+          <Input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+          />
+        </div>
+
+        <div>
+          <Label htmlFor="newName" className="mb-1 block text-sm font-medium">
+            nom
+          </Label>
+          <Input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Nom"
+          />
+        </div>
+
+        <Button type="submit" className="rounded px-4 py-2">
+          Creation d&apos;un user
+        </Button>
+
+        {message && (
+          <p
+            className={`mt-4 text-sm ${message.includes('succès') ? 'text-green-600' : 'text-red-600'}`}
+          >
+            {message}
+          </p>
+        )}
+      </form>
+    </div>
+  )
+}
