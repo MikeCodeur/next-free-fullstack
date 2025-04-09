129d8e37a3e65ccef6314c125680dcb055c5bb16
ons <onshamadkl@gmail.com>
2025-04-09 16:06:32 +0100

    migration next 15.2.5

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,20 @@
 /** @type {import('next').NextConfig} */
 
-import createMDX from '@next/mdx';
+import createMDX from '@next/mdx'
+import remarkGfm from 'remark-gfm'
+import rehypePrism from 'rehype-prism-plus'
+import remarkRehype from 'remark-rehype'
 
 const withMDX = createMDX({
   extension: /\.mdx?$/,
   options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [rehypePrism],
   },
-});
+})
 
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'md', 'ts', 'tsx'],
-};
+}
 
-export default withMDX(nextConfig);
+export default withMDX(nextConfig)
